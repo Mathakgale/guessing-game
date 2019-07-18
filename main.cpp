ce9705dc752acd7ac493285d715670a582d53438
Mathakgale <mathakgalemohale@gmail.com>
2019-07-18 22:20:06 +0200

    Random generator done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,62 @@
 
 using namespace std;
 
+class guessGame
+{
+
+    public :
+        guessGame()
+        {
+            temp = 0 ;
+        }
+        void play() ;
+        int getRand() ;
+    private :
+        int temp, RandNum ;
+
+
+};
+
+void guessGame::play()
+{
+    RandNum = getRand() ;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> temp ;
+
+
+        if (temp == RandNum)
+        {
+            cout <<"You win" <<endl ;
+            break ;
+        }
+        else if (temp > RandNum && i != 4)
+        {
+            cout <<"Guess lower" <<endl ;
+        }
+        else if (temp < RandNum && i != 4)
+        {
+            cout <<"Guess higher" <<endl ;
+        }
+        else
+        {
+            cout <<"You lost" <<endl ;
+        }
+    }
+
+}
+int guessGame::getRand()
+{
+    srand(time(0)) ;
+    RandNum = 1+rand()%101 ;
+    return RandNum ;
+}
+
 int main()
 {
 
+    guessGame guessing_game ;
+
+    guessing_game.play() ;
 }
