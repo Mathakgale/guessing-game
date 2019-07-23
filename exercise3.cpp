5ab4d14ebd82533329942497309bcd1a88f8d70e
Mathakgale <mathakgalemohale@gmail.com>
2019-07-23 17:52:26 +0200

    Answered exercise 3.1

--- exercise3.cpp
+++ exercise3.cpp
@@ -1,9 +1,11 @@
 #include <iostream>   // contains the definition of cout, endl
 #include <complex>   // contains the complex class definition
+
 using namespace std; // cout, endl, complex are all part of this namespace
-int main()
+
+int main(void)
 {
-    auto num1 = complex< float >{2.0, 2.0}; // use auto for type deduction
+    auto num1 = complex<float>{2.0, 2.0}; // use auto for type deduction
     auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax  (curly braces)
 
     auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
@@ -15,5 +17,8 @@ int main()
     << endl << endl;
 
     // answer++;
+    // answer++ cannot work because the complex class does not have a member 
+    //Function which is overloaded with the operator ++
     return 0;
 }
+
